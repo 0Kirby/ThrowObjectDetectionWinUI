c998e754095c694b17a3b4e5dd3eef4a64af2a9a
0Kirby <jty.1234567890@163.com>
2021-12-21 03:53:20 +0800

    Add SettingSection style

--- SettingsPage.xaml.cs
+++ SettingsPage.xaml.cs
@@ -27,8 +27,8 @@ namespace ThrowObjectDetection
             p.StartInfo.WorkingDirectory = @"C:\Users\0Kirby\PycharmProjects\MachineLearning\CarObjectDetectionTest\yolov5";
             p.StartInfo.FileName = @"C:\Users\0Kirby\PycharmProjects\MachineLearning\venv\Scripts\python.exe"; //虚拟环境中python的安装路径
             //p.StartInfo.Arguments = @"C:\Users\0Kirby\PycharmProjects\MachineLearning\CarObjectDetectionTest\yolov5\models\yolo.py --cfg C:\Users\0Kirby\PycharmProjects\MachineLearning\CarObjectDetectionTest\yolov5\models\yolov5n6-C3TR-CBAM-P2.yaml";
-            //p.StartInfo.Arguments = @"D:\hello.py";
-            p.StartInfo.Arguments = @"C:\Users\0Kirby\PycharmProjects\MachineLearning\CarObjectDetectionTest\yolov5\train.py --img 640 --batch 1 --epoch 2 --data data/car.yaml --cfg models/yolov5n.yaml --weights weights/yolov5n.pt";
+            p.StartInfo.Arguments = @"D:\hello.py";
+            //p.StartInfo.Arguments = @"C:\Users\0Kirby\PycharmProjects\MachineLearning\CarObjectDetectionTest\yolov5\train.py --img 640 --batch 1 --epoch 2 --data data/car.yaml --cfg models/yolov5n.yaml --weights weights/yolov5n.pt";
             p.StartInfo.UseShellExecute = false;
             p.StartInfo.RedirectStandardError = true;
             p.StartInfo.CreateNoWindow = true;
